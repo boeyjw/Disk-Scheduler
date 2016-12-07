8ceb0d6e3322975da3ad91a03f51db412eb06642
Boey <khcy5bjw@nottingham.edu.my>
2016-12-07 23:18:07 +0800

    Minor overhaul of interface. Requires testing and removal of horizontal scrolling on the AnchorPane where the graph resides

--- src/javafx/application/Controller.java
+++ src/javafx/application/Controller.java
@@ -1,6 +1,7 @@
 package javafx.application;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.ResourceBundle;
@@ -21,6 +22,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 import osc.diskscheduling.algorithm.ControllerBroker;
@@ -61,9 +63,16 @@ public class Controller implements Initializable {
 		private ComboBox<String> load_combo;
 		
 		
-		//text 
+		//description
+		@FXML
+		private Label desc_label;
 		@FXML
 		private TextArea description_text;
+		
+		//modified queue show text area
+		@FXML
+		private GridPane grid_pane;
+		//label grid, column 0
 		@FXML
 		private Label fcfs_label;
 		@FXML
@@ -76,24 +85,19 @@ public class Controller implements Initializable {
 		private Label look_label;
 		@FXML
 		private Label clook_label;
-		
-		//pane
+		//text area grid, column 1
 		@FXML
-		private Pane custom_pane;
+		private TextArea fcfs_textqueue;
 		@FXML
-		private Pane fcfs_pane;
+		private TextArea sstf_textqueue;
 		@FXML
-		private Pane sstf_pane;
+		private TextArea scan_textqueue;
 		@FXML
-		private Pane scan_pane;
+		private TextArea cscan_textqueue;
 		@FXML
-		private Pane cscan_pane;
+		private TextArea look_textqueue;
 		@FXML
-		private Pane look_pane;
-		@FXML
-		private Pane clook_pane;
-		@FXML
-		private Pane load_pane;
+		private TextArea clook_textqueue;	
 		
 		//graph
 		@FXML
@@ -106,17 +110,13 @@ public class Controller implements Initializable {
 		//parameters
 		private static String message="";
 		private static String message2="";
+		private static final String load_message="Pick a load type (Default: Custom)";
 		private LinkedList<Integer> queue = new LinkedList<Integer>();
 		private int cylinder;
 		private int head; 
 
 	    @FXML
 	    private LineChart<Integer, Integer> custom_linechart;
-	    
-	    //progress bar
-	    @FXML
-	    private ProgressBar load_bar;
-	    
 	    private static int seriesCounter = 0; 
 
         private ControllerBroker cbr = new ControllerBroker();
@@ -126,35 +126,65 @@ public class Controller implements Initializable {
 		@Override
 		public void initialize(URL location, ResourceBundle resources) {
 			load_combo.setItems(FXCollections.observableArrayList("Light","Medium","Heavy","Custom"));
-			load_combo.setValue("Pick a load type (Default: Custom)");
+			load_combo.setValue(load_message);
 			
-			fcfs_pane.setVisible(false);
-			sstf_pane.setVisible(false);
-			scan_pane.setVisible(false);
-			cscan_pane.setVisible(false);
-			look_pane.setVisible(false);
-			clook_pane.setVisible(false);
+			description_text.setEditable(false);
 			
 			xaxis.setLabel("Request");
 			yaxis.setLabel("Cylinder");
+			
+			grid_pane.setVisible(false);
+			
+			fcfs_label.setVisible(false);
+			sstf_label.setVisible(false);
+			scan_label.setVisible(false);
+			cscan_label.setVisible(false);
+			look_label.setVisible(false);
+			clook_label.setVisible(false);
+			
+			fcfs_textqueue.setVisible(false);
+			sstf_textqueue.setVisible(false);
+			scan_textqueue.setVisible(false);
+			cscan_textqueue.setVisible(false);
+			look_textqueue.setVisible(false);
+			clook_textqueue.setVisible(false);
+			
+			fcfs_textqueue.setEditable(false);
+			sstf_textqueue.setEditable(false);
+			scan_textqueue.setEditable(false);
+			cscan_textqueue.setEditable(false);
+			look_textqueue.setEditable(false);
+			clook_textqueue.setEditable(false);
 		}
 		
 		@FXML
 		private void load(){
 			String loadType = load_combo.getValue();
 			if(loadType.compareTo("Light") == 0){
+				cylinderamount.setEditable(false);
+				currenthead.setEditable(false);
+				jobreq.setEditable(false);
 				cylinderamount.setText("199");
 	    		currenthead.setText("50");
 	    		jobreq.setText("42,36,76,98,80,72,52,40,32,12");
 			}else if(loadType.compareTo("Medium") == 0){
+				cylinderamount.setEditable(false);
+				currenthead.setEditable(false);
+				jobreq.setEditable(false);
 				cylinderamount.setText("999");
 	    		currenthead.setText("500");
 	    		jobreq.setText("421,125,624,524,125,32,125,324,698,203,421,698,825,421");
 			}else if(loadType.compareTo("Heavy") == 0){
+				cylinderamount.setEditable(false);
+				currenthead.setEditable(false);
+				jobreq.setEditable(false);
 				cylinderamount.setText("999");
 	    		currenthead.setText("500");
 	    		jobreq.setText("987,123,896,214,987,123,998,2,875,365,993,2,785,12,987,56,865,12,998,123");
 			}else if(loadType.compareTo("Custom") == 0){
+				cylinderamount.setEditable(true);
+				currenthead.setEditable(true);
+				jobreq.setEditable(true);
 				cylinderamount.clear();
 				currenthead.clear();
 				jobreq.clear();
@@ -165,63 +195,80 @@ public class Controller implements Initializable {
 		//handle input algorithm option
 	    private void algorithmOptions(){
 	    	LinkedList<Integer> temp = new LinkedList<Integer>();
+	    	int[] totalSeekTime = new int[6];
+	    	String[] descShowAlgolSeek = new String[] {"FCFS", "SSTF", "Scan", "CScan", "Look", "CLook"};
+	    	Arrays.fill(totalSeekTime, -1);
 	    	boolean anySelected = false;
 	    	if(fcfs.isSelected()==true){
 	    		temp = cbr.algorithmSelector(queue, ControllerBroker.FCFS);
 	    		graphingData(temp, "First Come First Serve");
+	    		totalSeekTime[0] = temp.pop().intValue();
 	    		temp.pop();
-	    		temp.pop();
-	    		fcfs_label.setText(temp.toString());
-	    		fcfs_pane.setVisible(true);
+	    		fcfs_label.setVisible(true);
+	    		fcfs_textqueue.setVisible(true);
+	    		fcfs_textqueue.setText(temp.toString());
 	    		anySelected = true;
 	    	}
 	    	if(sstf.isSelected()==true){
 	    		temp = cbr.algorithmSelector(queue, ControllerBroker.SSTF);
 	    		graphingData(temp, "Shortest Seek Time First");
+	    		totalSeekTime[1] = temp.pop().intValue();
 	    		temp.pop();
-	    		temp.pop();
-	    		sstf_label.setText(temp.toString());
-				sstf_pane.setVisible(true);
+	    		sstf_label.setVisible(true);
+	    		sstf_textqueue.setVisible(true);
+	    		sstf_textqueue.setText(temp.toString());
 				anySelected = true;
 	    	}
 	    	if(scan.isSelected()==true){
 	    		temp = cbr.algorithmSelector(queue, ControllerBroker.SCAN);
 	    		graphingData(temp, "Scan");
+	    		totalSeekTime[2] = temp.pop().intValue();
 	    		temp.pop();
-	    		temp.pop();
-	    		scan_label.setText(temp.toString());
-				scan_pane.setVisible(true);
+	    		scan_label.setVisible(true);
+	    		scan_textqueue.setVisible(true);
+	    		scan_textqueue.setText(temp.toString());
 				anySelected = true;
 	    	}
 	    	if(cscan.isSelected()==true){
 	    		temp = cbr.algorithmSelector(queue, ControllerBroker.CSCAN);
 	    		graphingData(temp, "CScan");
+	    		totalSeekTime[3] = temp.pop().intValue();
 	    		temp.pop();
-	    		temp.pop();
-	    		cscan_label.setText(temp.toString());
-				cscan_pane.setVisible(true);
+	    		cscan_label.setVisible(true);
+	    		cscan_textqueue.setVisible(true);
+	    		cscan_textqueue.setText(temp.toString());
 				anySelected = true;
 	    	}
 	    	if(look.isSelected()==true){
 	    		temp = cbr.algorithmSelector(queue, ControllerBroker.LOOK);
 	    		graphingData(temp, "Look");
+	    		totalSeekTime[4] = temp.pop().intValue();
 	    		temp.pop();
-	    		temp.pop();
-	    		look_label.setText(temp.toString());
-				look_pane.setVisible(true);
+	    		look_label.setVisible(true);
+	    		look_textqueue.setVisible(true);
+	    		look_textqueue.setText(temp.toString());
 				anySelected = true;
 	    	}
 	    	if(clook.isSelected()==true){
 	    		temp = cbr.algorithmSelector(queue, ControllerBroker.CLOOK);
 	    		graphingData(temp, "CLook");
+	    		totalSeekTime[5] = temp.pop().intValue();
 	    		temp.pop();
-	    		temp.pop();
-	    		clook_label.setText(temp.toString());
-				clook_pane.setVisible(true);
+	    		clook_label.setVisible(true);
+	    		clook_textqueue.setVisible(true);
+	    		clook_textqueue.setText(temp.toString());
 				anySelected = true;
 	    	}
 	    	if(anySelected==true){
 	    		addAlgorithmSeries();
+	    		String desc_algolSeekText = "";
+	    		for(int i = 0; i < totalSeekTime.length; i++) {
+	    			if(totalSeekTime[i] != -1) {
+	    				desc_algolSeekText += descShowAlgolSeek[i] + ":\n" + "Total seek time: " + totalSeekTime[i] + "\n\n";
+	    			}
+	    		}
+	    		desc_label.setText("Algorithm information");
+	    		description_text.setText(desc_algolSeekText);
 	    	}
 	    }
 	    
@@ -264,7 +311,7 @@ public class Controller implements Initializable {
 					+ "Reset All to clear all parameters.\n"
 					+ "Simulate to run simulation.\n"
 					+des
-					);	
+				);	
 	    }
 	    
 	    
@@ -274,14 +321,12 @@ public class Controller implements Initializable {
 			alert.setTitle("Error Dialog");
 			alert.setContentText(message+"\n"+message2);
 			alert.showAndWait();
-			reset();
 		}
 	    
 	    
 		//to clear all values to default (null)
 	    @FXML
 		public void reset() {
-
 				resetInternal();
 								
 				cylinderamount.clear();
@@ -296,12 +341,19 @@ public class Controller implements Initializable {
 				cscan.setSelected(false);
 				look.setSelected(false);
 				clook.setSelected(false);
-								
+				
+				cylinderamount.setEditable(true);
+				currenthead.setEditable(true);
+				jobreq.setEditable(true);
+				
+				grid_pane.setVisible(false);
+					
                 message="";
                 message2="";
                 
+                desc_label.setText("Description");
+                load_combo.setValue(load_message);
 				printText("");
-				load_combo.setValue("Pick a load type");
 		}
 	    
 	    private void resetInternal(){
@@ -310,36 +362,53 @@ public class Controller implements Initializable {
 			queue.clear();
 			seriesCounter = 0;
 			
-			fcfs_pane.setVisible(false);
-			sstf_pane.setVisible(false);
-			scan_pane.setVisible(false);
-			cscan_pane.setVisible(false);
-			look_pane.setVisible(false);
-			clook_pane.setVisible(false);
+			fcfs_label.setVisible(false);
+			sstf_label.setVisible(false);
+			scan_label.setVisible(false);
+			cscan_label.setVisible(false);
+			look_label.setVisible(false);
+			clook_label.setVisible(false);
+			
+			fcfs_textqueue.setVisible(false);
+			sstf_textqueue.setVisible(false);
+			scan_textqueue.setVisible(false);
+			cscan_textqueue.setVisible(false);
+			look_textqueue.setVisible(false);
+			clook_textqueue.setVisible(false);
 	    }
 	    
 		//to pass parameters and run simulation
 	    @FXML
 		public void simulate(){
+	    	boolean isInvalid = false;
 	    	try{
+	    			grid_pane.setVisible(true);
 					resetInternal();
 	    			algorithmOptionCheck();	
 	        		cylinder = Integer.parseInt(cylinderamount.getText().trim());
 					head = Integer.parseInt(currenthead.getText().trim());
-					queue.add(head);
+					if(head > cylinder) {
+						isInvalid = true;
+					}
+					else {
+						queue.add(head);
+					}
 	        		for(String temp:jobreq.getText().replaceAll("\\s+", "").split(",")){
 	        			int foo = Integer.parseInt(temp);
-	        			if(foo>cylinder || foo<0){
-	        				message="Job request not in range of cylinder";
+	        			if(foo>cylinder || foo<0 || isInvalid){
+	        				message="Job request not in range of cylinder or current head is more than total number of cylinder";
 	        				errorMessage();
+	        				isInvalid = true;
+	        				break;
 	        			}
 	        			else
 	        				queue.add(Integer.parseInt(temp));
-	        			}
+	        		}
 	        		queue.add(cylinder);
-	        		algorithmOptions();	
-					customgraph_pane.setVisible(true);
-					printText("Kindly reset before new simulation.");					
+	        		if(!isInvalid) {
+	        			algorithmOptions();	
+						customgraph_pane.setVisible(true);	
+	        		}
         	}catch(NumberFormatException e){
         		message="Please make sure all text input is in integer.\nJob request must be filled and must only contain integers separated by comma.";
         		errorMessage();
