4251eb8763f79d86933ddf9ca16983797fc11184
Chia <chia96@live.com>
2016-12-07 15:46:40 +0800

    yay

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXML;
 //import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -9,22 +10,12 @@ import javafx.scene.Scene;
 //import javafx.scene.control.Button;
 //import javafx.scene.control.TextField;
 //import javafx.scene.control.Tooltip;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 
 public class Main extends Application{
-	
-	/*@FXML
-	private TextField cylinderamount;
-	@FXML
-	private TextField currenthead;
-	@FXML
-	private TextField jobreq;
-	@FXML 
-	private Button reset;
-	@FXML
-	private Button simulate;*/
-	
-	
+		
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		try {
@@ -36,12 +27,6 @@ public class Main extends Application{
 			primaryStage.setTitle("Disk Scheduling Simulator"); 					
 			primaryStage.show();
 			primaryStage.setResizable(true);
-			
-			/*cylinderamount.setTooltip(new Tooltip text = "Total number of cylinder on a disk."));
-			currenthead.setTooltip(new Tooltip("The cylinder where the head is currently."));
-			jobreq.setTooltip(new Tooltip("The job request queue in the cylinder."));
-			reset.setTooltip(new Tooltip("Clear all values."));
-			simulate.setTooltip(new Tooltip("Run simulation to display graph(s)."));*/
 			
 		} catch(Exception e) {
 			e.printStackTrace();
